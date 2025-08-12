6cc25a194a710135483c3ca57af1435c5931b9d2
Ankita Katyal <ankita.katyal@gainserv.com>
2025-08-12 14:13:19 +0530

    Changes in script

--- script.js
+++ script.js
@@ -1,30 +1,69 @@
+var uzeraq = [];
+function uzera(method, args) {
+  !!uzeraMethods && uzeraMethods[method]
+    ? uzeraMethods[method](args)
+    : uzeraq.push({ method: method, args: args });
+}
+(function (u, z, e, r, a) {
+  if (u["a_id"]) {
+    return;
+  }
+  u["a_id"] = a;
+  u["dm"] = u.location.hostname;
+  u[r] = u[r] || [];
+  u[e] = {};
+  var s = z.createElement("script");
+  s.async = true;
+  s.src = "https://assets.uzera.com/wrapper.js?v=" + Date.now();
+  var t = z.getElementsByTagName("script")[0];
+  t.parentNode.insertBefore(s, t);
+})(window, document, "uzeraMethods", "uzeraDL", "9D7OWRN47V");
 
-//Ashish+1 production
-// var uzeraq = []; function uzera(method, args) { (!!uzeraMethods && uzeraMethods[method]) ? uzeraMethods[method](args) : uzeraq.push({ method: method, args: args }); } (function (u, z, e, r, a) { if (u["a_id"]) { return; } u["a_id"] = a; u["dm"] = u.location.hostname; u[r] = u[r] || []; u[e] = {}; var s = z.createElement("script"); s.async = true; s.src = "https://assets.uzera.com/wrapper.js?v=" + Date.now(); var t = z.getElementsByTagName("script")[0]; t.parentNode.insertBefore(s, t); })(window, document, "uzeraMethods", "uzeraDL", "0CU192AF3L")
-//Ashish+7 production
-var uzeraq = []; function uzera(method, args) { (!!uzeraMethods && uzeraMethods[method]) ? uzeraMethods[method](args) : uzeraq.push({ method: method, args: args }); } (function (u, z, e, r, a) { if (u["a_id"]) { return; } u["a_id"] = a; u["dm"] = u.location.hostname; u[r] = u[r] || []; u[e] = {}; var s = z.createElement("script"); s.async = true; s.src = "https://assets.uzera.com/wrapper.js?v=" + Date.now(); var t = z.getElementsByTagName("script")[0]; t.parentNode.insertBefore(s, t); })(window, document, "uzeraMethods", "uzeraDL", "JS0A9XT00U")
-// ashish dev 
-// var uzeraq = []; function uzera(method, args) { (!!uzeraMethods && uzeraMethods[method]) ? uzeraMethods[method](args) : uzeraq.push({ method: method, args: args }); } (function (u, z, e, r, a) { if (u["a_id"]) { return; } u["a_id"] = a; u["dm"] = u.location.hostname; u[r] = u[r] || []; u[e] = {}; var s = z.createElement("script"); s.async = true; s.src = "https://assets.gainserv.in/wrapper.dev.js?v=" + Date.now(); var t = z.getElementsByTagName("script")[0]; t.parentNode.insertBefore(s, t); })(window, document, "uzeraMethods", "uzeraDL", "2MU8MDJXZ9")
-//ashish one dev
-//var uzeraq = []; function uzera(method, args) { (!!uzeraMethods && uzeraMethods[method]) ? uzeraMethods[method](args) : uzeraq.push({ method: method, args: args }); } (function (u, z, e, r, a) { if (u["a_id"]) { return; } u["a_id"] = a; u["dm"] = u.location.hostname; u[r] = u[r] || []; u[e] = {}; var s = z.createElement("script"); s.async = true; s.src = "https://assets.gainserv.in/wrapper.dev.js?v=" + Date.now(); var t = z.getElementsByTagName("script")[0]; t.parentNode.insertBefore(s, t); })(window, document, "uzeraMethods", "uzeraDL", "9NG6D2STEP")
-//Temp Jason wizard ashish QA
-//var uzeraq = []; function uzera(method, args) { (!!uzeraMethods && uzeraMethods[method]) ? uzeraMethods[method](args) : uzeraq.push({ method: method, args: args }); } (function (u, z, e, r, a) { if (u["a_id"]) { return; } u["a_id"] = a; u["dm"] = u.location.hostname; u[r] = u[r] || []; u[e] = {}; var s = z.createElement("script"); s.async = true; s.src = "https://assets.gainserv.com/wrapper.net.js?v=" + Date.now(); var t = z.getElementsByTagName("script")[0]; t.parentNode.insertBefore(s, t); })(window, document, "uzeraMethods", "uzeraDL", "FFQG8L16S2")
-
-if(window.location.href.includes('about.html')) {
- uzera("track", {
-   event: "About custom click", // Replace with your custom event name
-   data: {
-        },
- });
- console.log('about event trigger')
+if (window.location.href.includes("about.html")) {
+  uzera("track", {
+    event: "About custom click", // Replace with your custom event name
+    data: {},
+  });
+  console.log("about event trigger");
 }
 
 // intercom
 
-  window.intercomSettings = {
-    api_base: "https://api-iam.intercom.io",
-    app_id: "em4byeqt",
-  };
-  // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/em4byeqt'
-  (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',w.intercomSettings);}else{var d=document;var i=function(){i.c(arguments);};i.q=[];i.c=function(args){i.q.push(args);};w.Intercom=i;var l=function(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/em4byeqt';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);};if(document.readyState==='complete'){l();}else if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
-
+window.intercomSettings = {
+  api_base: "https://api-iam.intercom.io",
+  app_id: "em4byeqt",
+};
+// We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/em4byeqt'
+(function () {
+  var w = window;
+  var ic = w.Intercom;
+  if (typeof ic === "function") {
+    ic("reattach_activator");
+    ic("update", w.intercomSettings);
+  } else {
+    var d = document;
+    var i = function () {
+      i.c(arguments);
+    };
+    i.q = [];
+    i.c = function (args) {
+      i.q.push(args);
+    };
+    w.Intercom = i;
+    var l = function () {
+      var s = d.createElement("script");
+      s.type = "text/javascript";
+      s.async = true;
+      s.src = "https://widget.intercom.io/widget/em4byeqt";
+      var x = d.getElementsByTagName("script")[0];
+      x.parentNode.insertBefore(s, x);
+    };
+    if (document.readyState === "complete") {
+      l();
+    } else if (w.attachEvent) {
+      w.attachEvent("onload", l);
+    } else {
+      w.addEventListener("load", l, false);
+    }
+  }
+})();
