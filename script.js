22f52e534e4b6c17bd92bdcfca5086a5b9e43449
ankitaAtGit <48355393+ankitaAtGit@users.noreply.github.com>
2025-09-26 11:34:00 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
-var uzeraq = [];
-function uzera(method, args) {
-  !!uzeraMethods && uzeraMethods[method]
-    ? uzeraMethods[method](args)
-    : uzeraq.push({ method: method, args: args });
-}
-(function (u, z, e, r, a) {
-  if (u["a_id"]) {
-    return;
-  }
-  u["a_id"] = a;
-  u["dm"] = u.location.hostname;
-  u[r] = u[r] || [];
-  u[e] = {};
-  var s = z.createElement("script");
-  s.async = true;
-  s.src = "https://assets.uzera.com/wrapper.js?v=" + Date.now();
-  var t = z.getElementsByTagName("script")[0];
-  t.parentNode.insertBefore(s, t);
-})(window, document, "uzeraMethods", "uzeraDL", "9D7OWRN47V");
+
+var uzeraq = []; function uzera(method, args) { (!!uzeraMethods && uzeraMethods[method]) ? uzeraMethods[method](args) : uzeraq.push({ method: method, args: args }); } (function (u, z, e, r, a) { if (u["a_id"]) { return; } u["a_id"] = a; u["dm"] = u.location.hostname; u[r] = u[r] || []; u[e] = {}; var s = z.createElement("script"); s.async = true; s.src = "https://assets.gainserv.in/wrapper.dev.js?v=" + Date.now(); var t = z.getElementsByTagName("script")[0]; t.parentNode.insertBefore(s, t); })(window, document, "uzeraMethods", "uzeraDL", "I84N7SQKE7")
+
 
 if (window.location.href.includes("about")) {
   uzera("track", {
